8efec02fe1379e203b883159f68f4c689c9509b2
Cristian Torrejon <c.torrejon96@gmail.com>
2023-10-30 16:09:49 -0300

    Se creo el proyecto y se agregaron las dependencias necesarias

---
+++ src/screens/home/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white">Home Screen</Text>
+    </View>
+  )
+}
